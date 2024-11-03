08f9d603be98021c96152c790105d44083104f10
emad-mohamadi <seyedemadmohammadi@gmail.com>
2024-11-03 21:49:57 +0330

    __

--- navigate.py
+++ navigate.py
@@ -1,43 +1,113 @@
-from display import Screen, Window, welcome, sleep, randint, borders, format
+from random import randint, choice
 
-game_size = 15
-bounded = False
-break_time = 0.1
 
-if bounded:
-    game_size += 2
-new_win = Window((game_size*2, game_size))
-new_win.set_board(game_size)
-if bounded:
-    new_win.board.set(*[(0, i) for i in range(game_size)])
-    new_win.board.set(*[(i, 0) for i in range(game_size)])
-    new_win.board.set(*[(game_size-1, i) for i in range(game_size)])
-    new_win.board.set(*[(i, game_size-1) for i in range(game_size)])
-    new_win.set_border(*borders["no-border"])
-else:
-    new_win.set_border(*borders["rounded"])
+class Board:
+    def __init__(self, size, value=0):
+        self.size = size
+        self.mat = [[value] * size for _ in range(size)]
+        return
 
-snake_body = [new_win.board.drop_apple()]
-new_win.board.set(*snake_body, value=1)
-new_win.apple = new_win.board.drop_apple()
-scr = Screen()
+    def drop_apple(self):
+        new_apple = (randint(0, self.size-1), randint(0, self.size-1))
+        while self.is_blocked(new_apple):
+            new_apple = (randint(0, self.size-1), randint(0, self.size-1))
+        return new_apple
 
-while True:
-    new_win.set_header(title=str(len(snake_body)))
-    scr.add_window(new_win)
-    scr.show()
-    scr.clear()
-    sleep(break_time)
-    steps = new_win.board.find_path(snake_body[-1], new_win.apple)
-    if not steps:
-        print("GAME-OVER")
-        break
-    next_step = steps.pop()
-    new_win.path = steps
-    if next_step == new_win.apple:
-        new_win.apple = new_win.board.drop_apple()
-    else:
-        new_win.board.set(snake_body.pop(0), value=0)
-        new_win.board.minus(*snake_body)
-    snake_body.append(next_step)
-    new_win.board.set(next_step, value=len(snake_body))
+    def set(self, *positions, value=-1):
+        for position in positions:
+            self.mat[position[0]][position[1]] = value
+        return
+
+    def minus(self, *positions):
+        for position in positions:
+            self.mat[position[0]][position[1]] -= 1
+        return
+
+    def is_blocked(self, position, distance=0):
+        return False if 0 <= self.mat[position[0]][position[1]] <= distance else True
+        # return self.mat[position[0]][position[1]]
+
+    def __repr__(self):
+        for _ in range(self.size):
+            return ('\n'.join(["".join(
+                [f"{self.mat[i][j]: 3}" for j in range(self.size)]) for i in range(self.size)]))
+
+    # Classic
+    def neighbors(self, pos, visited=None, distance=0):
+        if not visited:
+            visited = self
+
+        def fix(n):
+            if 0 <= n < self.size:
+                return n
+            elif n < 0:
+                return n + self.size
+            else:
+                return n % self.size
+
+        positions = [(fix(pos[0]+1), pos[1]), (fix(pos[0]-1), pos[1]),
+                     (pos[0], fix(pos[1]+1)), (pos[0], fix(pos[1]-1))]
+        return [position for position in positions if not (self.is_blocked(position, distance) or visited.is_blocked(position))]
+
+    def find_path(self, start, target):
+        path = {}
+        queue = Heap()
+        visited = Board(self.size)
+        distances = {
+            start: 0
+        }
+        for neighbor in self.neighbors(start, distance=1):
+            queue.push((neighbor, 1))
+            path[neighbor] = start
+        visited.set(start)
+        while queue.data:
+            pos, distance = queue.pop()
+
+            if pos == target:
+                break
+            if visited.is_blocked(pos):
+                continue
+            visited.set(pos)
+            distances[pos] = distance
+            for neighbor in self.neighbors(pos, visited, distance):
+                queue.push((neighbor, distance + 1))
+                path[neighbor] = pos
+
+        try:
+            try:
+                step = target
+                steps = [step]
+                while path[step] != start:
+                    step = path[step]
+                    steps.append(step)
+                return steps
+            except KeyError:
+                return [choice(self.neighbors(start, Board(self.size), distance=1))]
+        except:
+            return None
+
+
+class Heap:
+    def __init__(self):
+        self.data = []
+        return
+
+    def push(self, *pairs):
+        for pair in pairs:
+            self.data.append(pair)
+        self.data.sort(key=lambda a: a[1])
+        return
+
+    def pop(self):
+        return self.data.pop(0)
+
+
+# board = Board(20)
+# board.set(*[(8, i) for i in range(17)], value=1)
+# board.set(*[(16, i) for i in range(5, 20)], value=1)
+# board.set(*[(0, i) for i in range(20)], value=1)
+# board.set(*[(i, 0) for i in range(20)], value=1)
+# board.set(*[(20-1, i) for i in range(20)], value=1)
+# board.set(*[(i, 20-1) for i in range(20)], value=1)
+# board.set(*board.find_path((1, 1), (18, 18)))
+# print(board)
