b461a3d1516c85fea7fd1b102aa999fe561b417c
emad-mohamadi <seyedemadmohammadi@gmail.com>
2024-11-27 14:12:23 +0330

    working directory bug fixed

--- main.py
+++ main.py
@@ -1,16 +1,25 @@
-from logic import Game, sleep
-from display import welcome, Screen, format
+def main():
+    from logic import Game, sleep
+    from display import welcome, Screen, format
+    from os import path
 
-with open("logo.txt", "r") as logo:
-    print(logo.read())
-sleep(1)
-welcome(name="marboro")
+    directory = path.dirname(path.abspath(__file__))
 
-snake = Game(size=16, wall=True, autopilot=True, show_path=True)
+    with open(fr"{directory}\logo.txt", "r") as logo:
+        print(logo.read())
+    sleep(1)
+    welcome(name="marboro")
 
-code = 3
-while code:
-    code = snake.switch(code)
+    snake = Game(size=16, wall=True, autopilot=True, show_path=True)
+    snake.data_path = fr"{directory}\data.json"
 
-scr = Screen()
-scr.show(end=format["from-beginning"])
+    code = 3
+    while code:
+        code = snake.switch(code)
+
+    scr = Screen()
+    scr.show(end=format["from-beginning"])
+
+
+if __name__ == "__main__":
+    main()
